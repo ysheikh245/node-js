94422f1af3b18a062f96f93c43d7413dd87f8199
Yusuf Sheikh <v102580@pm.local>
2022-05-31 18:07:06 -0400

    Updating nodejs learning notes

---
+++ 1basics/anonymous-function.js
@@ -0,0 +1,3 @@
+module.exports = (number) => {
+    console.log(`Inside anonymous function. Number is ${number}`)
+};
--- 1basics/backtick.js
+++ 1basics/backtick.js
@@ -3,3 +3,8 @@ const name = 'Yusuf'
 const age = 40;
 
 console.log(`Found someone with name=${name} and age=${age}`)
+
+
+// Trying to import anonymous function here
+const anonymous = require('./anonymous-function');
+anonymous(5)
